1fcd9bb9f347638ff19393ff9f00a72b71a03b2f
Roman Pinchuk <hi@thisroman.dev>
2023-06-15 00:23:04 +0300

    Fixed div tag closing and added new Roboto font and set new margins for pdf

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-const Handlebars = require('handlebars');
-const { readFileSync, readdirSync } = require('fs');
-const { join } = require('path');
+const Handlebars = require("handlebars");
+const { readFileSync, readdirSync } = require("fs");
+const { join } = require("path");
 
-const HELPERS = join(__dirname, 'theme/hbs-helpers');
+const HELPERS = join(__dirname, "theme/hbs-helpers");
 
-const { birthDate } = require(join(HELPERS, 'birth-date.js'));
-const { dateHelpers } = require(join(HELPERS, 'date-helpers.js'));
-const { paragraphSplit } = require(join(HELPERS, 'paragraph-split.js'));
-const { toLowerCase } = require(join(HELPERS, 'to-lower-case.js'));
-const { spaceToDash } = require(join(HELPERS, 'space-to-dash.js'));
+const { birthDate } = require(join(HELPERS, "birth-date.js"));
+const { dateHelpers } = require(join(HELPERS, "date-helpers.js"));
+const { paragraphSplit } = require(join(HELPERS, "paragraph-split.js"));
+const { toLowerCase } = require(join(HELPERS, "to-lower-case.js"));
+const { spaceToDash } = require(join(HELPERS, "space-to-dash.js"));
 
 const { MY, Y, DMY } = dateHelpers;
 
-Handlebars.registerHelper('birthDate', birthDate);
-Handlebars.registerHelper('MY', MY);
-Handlebars.registerHelper('Y', Y);
-Handlebars.registerHelper('DMY', DMY);
-Handlebars.registerHelper('paragraphSplit', paragraphSplit);
-Handlebars.registerHelper('toLowerCase', toLowerCase);
-Handlebars.registerHelper('spaceToDash', spaceToDash);
+Handlebars.registerHelper("birthDate", birthDate);
+Handlebars.registerHelper("MY", MY);
+Handlebars.registerHelper("Y", Y);
+Handlebars.registerHelper("DMY", DMY);
+Handlebars.registerHelper("paragraphSplit", paragraphSplit);
+Handlebars.registerHelper("toLowerCase", toLowerCase);
+Handlebars.registerHelper("spaceToDash", spaceToDash);
 
 function render(resume) {
-  const css = readFileSync(`${__dirname}/style.css`, 'utf-8');
-  const tpl = readFileSync(`${__dirname}/resume.hbs`, 'utf-8');
-  const partialsDir = join(__dirname, 'theme/partials');
+  const css = readFileSync(`${__dirname}/style.css`, "utf-8");
+  const tpl = readFileSync(`${__dirname}/resume.hbs`, "utf-8");
+  const partialsDir = join(__dirname, "theme/partials");
   const filenames = readdirSync(partialsDir);
 
   filenames.forEach((filename) => {
@@ -31,7 +31,7 @@ function render(resume) {
     if (!matches) return;
     const name = matches[1];
     const filepath = join(partialsDir, filename);
-    const template = readFileSync(filepath, 'utf8');
+    const template = readFileSync(filepath, "utf8");
     Handlebars.registerPartial(name, template);
   });
 
@@ -41,14 +41,15 @@ function render(resume) {
   });
 }
 
-const marginValue = '0.8 cm';
 const pdfRenderOptions = {
+  format: "A4",
+  printBackground: true,
   margin: {
-    top: marginValue,
-    bottom: marginValue,
-    left: marginValue,
-    right: marginValue,
-  }
-}
+    bottom: "20px",
+    left: "25px",
+    right: "20px",
+    top: "20px",
+  },
+};
 
 module.exports = { render, pdfRenderOptions };
